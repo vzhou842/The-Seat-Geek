ef350d373f16b21285b34950ac7309b68ba42507
Victor Zhou <vzhou842@gmail.com>
2016-11-13 01:39:55 -0500

    extracted out seatgeek client id

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var SEATGEEK_CLIENT_ID = 'NjE5NTkwOHwxNDc5MDE1MzQ0';
+
 // Start listening.
 var port = process.env.PORT || 3500;
 var server = app.listen(port);
@@ -16,7 +18,7 @@ app.post('/message', twilio.webhook({ validate : false }), function(req, res, ne
 	var resp = new twilio.TwimlResponse();
 
     var request = require('request');
-    request('https://api.seatgeek.com/2/events?client_id=NjE5NTkwOHwxNDc5MDE1MzQ0', 
+    request('https://api.seatgeek.com/2/events?client_id=' + SEATGEEK_CLIENT_ID, 
         function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
