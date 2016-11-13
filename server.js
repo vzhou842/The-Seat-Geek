928b583bb5faf56f57f9ea8bd431a6113438f907
andrew7jin <andrew7jin@gmail.com>
2016-11-13 01:02:19 -0500

    tested seatgeak api

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ console.log('Server listening on port ' + port);
 
 app.post('/message', twilio.webhook({ validate : false }), function(req, res, next) {
 	var body = req.body.Body.trim();
+
 	var resp = new twilio.TwimlResponse();
-  	resp.message(body);
-    res.send(resp);
+
+    var request = require('request');
+    request('https://api.seatgeek.com/2/events?client_id=NjE5NTkwOHwxNDc5MDE1MzQ0', 
+        function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            resp.message(body);
+            res.send(resp);
+        }
+    })
 });
