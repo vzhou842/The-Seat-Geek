19ca2e27927a04273f581979982ab3eedd70f65d
Victor Zhou <vzhou842@gmail.com>
2016-11-13 00:15:22 -0500

    added POST /message route

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ var app = express();
 var port = process.env.PORT || 3500;
 var server = app.listen(port);
 console.log('Server listening on port ' + port);
+
+app.post('/message', function(req, res, next) {
+	var body = req.body.Body.trim();
+	console.log('body', body);
+});
