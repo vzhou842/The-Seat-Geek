1036bc659e5180dd422e6d1427428531a710f7c9
andrew7jin <andrew7jin@gmail.com>
2016-11-13 03:32:19 -0500

    changed zip to city

--- locationevents.js
+++ locationevents.js
@@ -1,15 +1,16 @@
 var message = 'What are events happening around me';
 
 
-var zip = '77055';
+var city = 'los angeles';
 var event;
 var client_id = 'NjE5NTkwOHwxNDc5MDE1MzQ0'
 
 var request = require('request');
 var shorten = require('./linkshortener');
 
-function getNearbyEvents(zip, callback){
-    request('https://api.seatgeek.com/2/events?geoip=' + zip + '&sort=score.desc&client_id=' + client_id, function (error, response, body) {
+function getNearbyEvents(city, callback){
+
+    request('https://api.seatgeek.com/2/events?venue.city=' + city + '&sort=score.desc&client_id=' + client_id, function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
             body = JSON.parse(body);
@@ -17,7 +18,7 @@ function getNearbyEvents(zip, callback){
             var eventNames = body.events.map(function(o) { return o.title; });
             var eventUrls = body.events.map(function(o) { return o.url; });
             shorten(eventUrls, function(urls) {
-                var message = 'Here are some events happening in ' + body.events[0].venue.city + ':';
+                var message = 'Here are some popular events happening in ' + body.events[0].venue.city + ':';
                 eventNames.forEach(function(title, i) {
                     message += '\n' + (i + 1) + '. ' + title + ': ' + urls[i];
                 });
@@ -30,7 +31,7 @@ function getNearbyEvents(zip, callback){
     })
 }
 
-getNearbyEvents(zip, console.log)
+getNearbyEvents(city, console.log)
 
 module.exports = {
     getNearbyEvents: getNearbyEvents
